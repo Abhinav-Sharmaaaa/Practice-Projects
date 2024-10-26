21e546865a8048c15059e280d8742f3a5b74bf15
Abhinav-Sharmaaaa <156526721+Abhinav-Sharmaaaa@users.noreply.github.com>
2024-10-26 21:32:21 +0530

    initial commit

---
+++ loginapp/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
